032771ac95fb664642de22d682d57af6dc27f02d
Bastian <104389376+Bastian-Community@users.noreply.github.com>
2022-08-28 23:35:36 +0200

    Final Bot

---
+++ Events/Interactions/SlashCommands.js
@@ -0,0 +1,28 @@
+const { ChatInputCommandInteraction } = require("discord.js");
+
+module.exports = {
+  name: "interactionCreate",
+
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command)
+      return interaction.reply({
+        content: "This command is outdated",
+        ephemeral: true,
+      });
+
+    if (command.developer && interaction.user.id !== "310382316339855363")
+      return interaction.reply({
+        content: "This command is only available for developers.",
+        ephemeral: true,
+      });
+
+    command.execute(interaction, client);
+  },
+};
